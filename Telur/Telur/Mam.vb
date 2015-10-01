5928cacfd47980e2763972e39582538b3dd7e8dc
Saraswati KurniaR <sasaysetiawan@gmail.com>
2015-10-01 17:26:00 +0700

    Update Simulation

---
+++ Telur/Telur/Mam.vb
@@ -0,0 +1,10 @@
+﻿Public Class Mam
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles touchButton.Click
+        If Me.BackColor = Color.White Then
+            Me.BackColor = Color.Firebrick
+        Else
+            Me.BackColor = Color.White
+        End If
+    End Sub
+End Class
